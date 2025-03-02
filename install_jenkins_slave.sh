c1ae9798f19539ce77ba04ab53b4d650ab5783e9
PRAVEEN KUMAR <97307468+praveenece431@users.noreply.github.com>
2025-03-02 09:14:43 +0530

    tf demo

--- install_jenkins_slave.sh
+++ install_jenkins_slave.sh
@@ -2,8 +2,8 @@
 sudo apt update -y
 sudo apt install openjdk-11-jdk -y
 sudo useradd -m -s /bin/bash jenkins-slave
-echo "jenkins-slave:password" | sudo chpasswd
-sudo mkdir /home/jenkins-slave/.ssh
-echo "ssh-rsa AAAA..." > /home/jenkins-slave/.ssh/authorized_keys
-sudo chown -R jenkins-slave:jenkins-slave /home/jenkins-slave/.ssh
-sudo chmod 600 /home/jenkins-slave/.ssh/authorized_keys
+#echo "jenkins-slave:password" | sudo chpasswd
+#sudo mkdir /home/jenkins-slave/.ssh
+#echo "ssh-rsa AAAA..." > /home/jenkins-slave/.ssh/authorized_keys
+#sudo chown -R jenkins-slave:jenkins-slave /home/jenkins-slave/.ssh
+#sudo chmod 600 /home/jenkins-slave/.ssh/authorized_keys
