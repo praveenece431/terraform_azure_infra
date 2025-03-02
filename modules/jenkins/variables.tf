25bcaefbfe3b114a504c44e7922d6e41dfce5197
PRAVEEN KUMAR <97307468+praveenece431@users.noreply.github.com>
2025-03-02 08:08:16 +0530

    tf demo

--- modules/jenkins/variables.tf
+++ modules/jenkins/variables.tf
@@ -1,8 +1,3 @@
-variable "vm_public_ips" {
-  description = "List of public IP addresses for the VMs"
-  type        = list(string)
-}
-
 variable "ssh_private_key" {
   description = "Private SSH key for connecting to VMs"
   type        = string
