5817909dcda162933c010ddc3e1166a70d0866a2
PRAVEEN KUMAR <97307468+praveenece431@users.noreply.github.com>
2025-03-02 07:53:38 +0530

    tf demo

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -1,29 +1,42 @@
 resource "null_resource" "jenkins_master" {
-  depends_on = [var.vm_master_id]
-
-  connection {
-    type        = "ssh"
-    user        = "azureuser"
-    private_key = file("~/.ssh/id_rsa")
-    host        = var.master_ip
-  }
-
   provisioner "remote-exec" {
-    script = "${path.module}/install_jenkins_master.sh"
+    connection {
+      type        = "ssh"
+      user        = "azureuser"
+      private_key = tls_private_key.ssh_key.private_key_pem # ✅ Use dynamically generated private key
+      host        = azurerm_linux_virtual_machine.vm[0].public_ip_address
+    }
+
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y openjdk-11-jdk",
+      "wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -",
+      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "sudo apt update -y",
+      "sudo apt install -y jenkins",
+      "sudo systemctl start jenkins",
+      "sudo systemctl enable jenkins"
+    ]
   }
 }
 
 resource "null_resource" "jenkins_slave" {
-  depends_on = [null_resource.jenkins_master]
-
-  connection {
-    type        = "ssh"
-    user        = "azureuser"
-    private_key = file("~/.ssh/id_rsa")
-    host        = var.slave_ip
-  }
-
   provisioner "remote-exec" {
-    script = "${path.module}/install_jenkins_slave.sh"
+    connection {
+      type        = "ssh"
+      user        = "azureuser"
+      private_key = tls_private_key.ssh_key.private_key_pem # ✅ Use dynamically generated private key
+      host        = azurerm_linux_virtual_machine.vm[1].public_ip_address
+    }
+
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y openjdk-11-jdk",
+      "sudo useradd -m -s /bin/bash jenkins-slave",
+      "sudo mkdir -p /home/jenkins-slave/.ssh",
+      "sudo echo '${tls_private_key.ssh_key.public_key_openssh}' > /home/jenkins-slave/.ssh/authorized_keys",
+      "sudo chown -R jenkins-slave:jenkins-slave /home/jenkins-slave/.ssh",
+      "sudo chmod 600 /home/jenkins-slave/.ssh/authorized_keys"
+    ]
   }
 }
